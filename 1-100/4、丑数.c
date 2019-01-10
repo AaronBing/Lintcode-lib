03f7ad44941ce2130f16dc3e694e5811d92fbcd4
AaronBing <15626187207@163.com>
2019-01-10 21:14:00 +0800

    4丑丑数完成

--- 1-100/4、丑数.c
+++ 1-100/4、丑数.c
@@ -1,54 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//
-//class Solution {
-//	public:
+
     /**
      * @param n: An integer
      * @return: the nth prime number as description.
      *///任何丑数都可以表示为：i^2 * j^3 * k^5；后一个丑数等于前面某个丑数乘以2或3或5：
     int nthUglyNumber(int n) {
-    	int numvalue[100];numvalue[0]=1; 
-    	
-        int num,num2=0,num3=0,num5=0;// 表示第几位数值
-        
+    
+    	int numvalue[2000];
+		numvalue[1]=1; 
+   	
+        int num,num2=1,num3=1,num5=1;// 表示第几位数值        
 		int numvalue2=0,numvalue3=0,numvalue5=0;//表示当前的数值  由位数num*各自的值 
 		
-//		numvalue2=num2*2; 				//先求原始初值 
-//		numvalue3=num3*3; 
-//		numvalue5=num5*5; 
 		
-
+		if(n==1) return 1;
 		
 		
 		for(num=2;num<=n;num++){//从第二个开始
+
+
+			numvalue2=numvalue[num2]*2;
+			numvalue3=numvalue[num3]*3;
+			numvalue5=numvalue[num5]*5;
+			//printf("初始numvalue2=%d,numvalue3=%d,numvalue5=%d\n",numvalue2,numvalue3,numvalue5);
 			
-			if(numvalue2==numvalue3)	++num2;	//保证不要重复先 
-			if(numvalue2==numvalue5)	++num2;
-			if(numvalue3==numvalue5)	++num3;
-			
-			numvalue2=num2*2; 				//先求原始初值 
-			numvalue3=num3*3; 
-			numvalue5=num5*5;
-			
-						 
-			if((numvalue2<=numvalue3)&&(numvalue2<=numvalue5)){		//首先判断3个数值哪个最小   从value2  到 value3 到  value5 
-				numvalue[num]=numvalue2;
-				numvalue2=(++num2)*2;
-			}else if((numvalue3<=numvalue2)&&(numvalue3<=numvalue5)){
-				numvalue[num]=numvalue3;
-				numvalue3=(++num3)*3;
-			}else if((numvalue5<=numvalue2)&&(numvalue5<=numvalue3)){
-				numvalue[num]=numvalue5;
-				numvalue5=(++num5)*5;
+			if(numvalue2==numvalue3)	{++num2;numvalue2=numvalue[num2]*2;}	   //保证不要重复先 
+			if(numvalue2==numvalue5)	{++num2;numvalue2=numvalue[num2]*2;}
+			if(numvalue3==numvalue5)	{++num3;numvalue3=numvalue[num3]*3;}			
+			 
+			if((numvalue[num2]*2<=numvalue[num3]*3)&&(numvalue[num2]*2<=numvalue[num5]*5)){		//首先判断3个数值哪个最小   从value2  到 value3 到  value5 
+				numvalue[num]=numvalue[num2]*2;
+				num2++;
+			}else if((numvalue[num3]*3<=numvalue[num2]*2)&&(numvalue[num3]*3<=numvalue[num5]*5)){
+				numvalue[num]=numvalue[num3]*3;
+				num3++;
+			}else if((numvalue[num5]*5<=numvalue[num2]*2)&&(numvalue[num5]*5<=numvalue[num3]*3)){
+				numvalue[num]=numvalue[num5]*5;
+				num5++;
 			}							
 		}
 		
-		return numvalue[n];
-		// write your code here
+		return numvalue[num-1];
+		
     }
-//};
+
     
     
     
